fb8c852306cbf29560035610d0360c30a1e0a1bf
geraldMaboshe <maboshegerald1@gmail.com>
2019-07-02 10:44:54 +0200

    Revert "refactored code"
    This reverts commit 36a8b34b8474f7b3ee8a5062e71adeffe0db33ac.

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ class App extends Component {
   state = {
     main: []
   };
-
-  // I coudn't figure out how to make the markComplete cleaner 😢 
+  // this can still be improved
+  // the difference between the earlier method is that this doesnt directly access this.state
+  // it changes the state it is getting, find a way of making this
+  // figure out a way of making this more cleaner 😉
   markComplete = (e, id) => {
     this.setState(state => ({
       main: state.main.map(list => {
@@ -21,26 +23,23 @@ class App extends Component {
   };
 
   componentDidMount() {
-
-  try{
+    // There is a lot improvement that you should here as well
+    //  make sure you catch errors that might happen
+    // an example is a network error, try and turn off internet and see what happens
     axios
       .get("https://jsonplaceholder.typicode.com/todos")
-      .then(res => this.setState(state =>({
-        main: res.data 
-      })));
-}catch(e){
-      alert("An eeror occured. It could be that you're offline")
-}
+      .then(res => this.setState({ main: res.data }));
   }
 
-
+  // leverage conditional rendering more
+  //  example is a loading message added that will display when the todos haven't loaded yet
   render() {
     return (
       <div className="App">
         {this.state.main.length ? (
           <Todo value={this.state.main} markComplete={this.markComplete} />
         ) : (
-          <h5>Loading todos...Please wait!!!</h5>
+          <h4>Loading ...</h4>
         )}
       </div>
     );
