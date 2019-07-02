ab7d6ce52953ae5b634bab5305f3c77f9aa0ed06
geraldMaboshe <maboshegerald1@gmail.com>
2019-07-02 12:07:11 +0200

    got rid of duplicates

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ class App extends Component {
   state = {
     main: []
   };
-  // this can still be improved
-  // the difference between the earlier method is that this doesnt directly access this.state
-  // it changes the state it is getting, find a way of making this
-  // figure out a way of making this more cleaner 😉
+
+  // I coudn't figure out how to make the markComplete cleaner 😢 
   markComplete = (e, id) => {
     this.setState(state => ({
       main: state.main.map(list => {
@@ -23,23 +21,26 @@ class App extends Component {
   };
 
   componentDidMount() {
-    // There is a lot improvement that you should here as well
-    //  make sure you catch errors that might happen
-    // an example is a network error, try and turn off internet and see what happens
+
+  try{
     axios
       .get("https://jsonplaceholder.typicode.com/todos")
-      .then(res => this.setState({ main: res.data }));
+      .then(res => this.setState(state =>({
+        main: res.data 
+      })));
+}catch(e){
+      alert("An eeror occured. It could be that you're offline")
+}
   }
 
-  // leverage conditional rendering more
-  //  example is a loading message added that will display when the todos haven't loaded yet
+
   render() {
     return (
       <div className="App">
         {this.state.main.length ? (
           <Todo value={this.state.main} markComplete={this.markComplete} />
         ) : (
-          <h4>Loading ...</h4>
+          <h5>Loading todos...Please wait!!!</h5>
         )}
       </div>
     );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Item from "./Item";
 
-// also made this a function
-// you only need a class when there is state update involved
-//  and if you want to get creative you can forget classes altogether
 function Todo(props) {
   return props.value.map(list => (
     <Item
